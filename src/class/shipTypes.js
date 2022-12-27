c592b95927327e4f605a577f19ba1a58ab02d5fa
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-27 15:15:58 +0300

    Add shipTypes.js

---
+++ src/class/shipTypes.js
@@ -0,0 +1,22 @@
+export default shipTypes = [
+  {
+    name: 'carrier',
+    length: 5,
+  },
+  {
+    name: 'battleship',
+    length: 4,
+  },
+  {
+    name: 'cruiser',
+    length: 3,
+  },
+  {
+    name: 'submarine',
+    length: 3,
+  },
+  {
+    name: 'destroyer',
+    length: 2,
+  },
+];
